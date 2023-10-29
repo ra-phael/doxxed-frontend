ae9ec56fedc4c32445a9dfc7ba73983e09294433
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2023-10-29 18:41:26 +0000

    add FAQ

---
+++ src/components/faq.tsx
@@ -0,0 +1,63 @@
+import { Disclosure } from "@headlessui/react";
+import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
+
+const faqs = [
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "What's the best thing about Ireland?",
+    answer:
+      "The population growth in their capital, every day it's dublin. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  // More questions...
+];
+
+export default function FAQ() {
+  return (
+    <div className="mx-auto w-8/12 px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
+      <div className="mx-auto divide-y divide-white/10">
+        <h2 className="text-2xl font-bold leading-10 tracking-tight text-white">
+          Frequently Asked Questions
+        </h2>
+        <dl className="mt-10 space-y-6 divide-y divide-white/10">
+          {faqs.map((faq) => (
+            <Disclosure as="div" key={faq.question} className="pt-6">
+              {({ open }) => (
+                <>
+                  <dt>
+                    <Disclosure.Button className="flex w-full items-start justify-between text-left text-white">
+                      <span className="text-base font-semibold leading-7">
+                        {faq.question}
+                      </span>
+                      <span className="ml-6 flex h-7 items-center">
+                        {open ? (
+                          <MinusSmallIcon
+                            className="h-6 w-6"
+                            aria-hidden="true"
+                          />
+                        ) : (
+                          <PlusSmallIcon
+                            className="h-6 w-6"
+                            aria-hidden="true"
+                          />
+                        )}
+                      </span>
+                    </Disclosure.Button>
+                  </dt>
+                  <Disclosure.Panel as="dd" className="ml-6 mt-2 pr-12">
+                    <p className="text-base text-left leading-7 text-gray-300">
+                      {faq.answer}
+                    </p>
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          ))}
+        </dl>
+      </div>
+    </div>
+  );
+}
